375fee0efeb0c0167b3ec0b0a8cad32d537f77d1
unknown <alexandruionut6@yahoo.com>
2024-01-13 18:30:08 +0200

    Login/Signup

--- front-end-react/src/App.js
+++ front-end-react/src/App.js
@@ -1,10 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
+import LoginSignup from './Components/LoginSignup/LoginSignup';
 
 function App() {
   return (
     <div>
-      <h1>Proiect WebTech</h1>
+      <LoginSignup/>
     </div>
   );
 }
